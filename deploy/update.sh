117b52a3cf0b4b79ab09c11c6e106f617abfd6f8
kibet masai <humphreykibet@gmail.com>
2022-10-13 02:48:30 +0300

    updated update.sh hkm

--- deploy/update.sh
+++ deploy/update.sh
@@ -5,6 +5,9 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/BIMS'
 
 git pull
+# Install python packages
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart BIMS_api
