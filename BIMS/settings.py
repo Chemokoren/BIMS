a08f9019c95c3d4e82d9eda6c0b9cc6c05c3cff5
kibet masai <humphreykibet@gmail.com>
2022-10-13 18:05:21 +0300

    updated settings hkm

--- BIMS/settings.py
+++ BIMS/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = ['ec2-54-205-41-54.compute-1.amazonaws.com',
-                 '54.205.41.54',
-                 '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
