33f49f5a2cc88392e353e8299eabd482bd7cda99
kibet masai <humphreykibet@gmail.com>
2022-10-13 04:19:55 +0300

    gunicorn.service,gunicorn.socket hkm

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 
 source env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/app
+cd /var/lib/jenkins/workspace/django-cicd/BIMS
 
 python3 manage.py makemigrations
 python3 manage.py migrate
